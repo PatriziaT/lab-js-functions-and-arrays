f47ed024a8269ae872f7ace0cd6019a3e61dafe0
Patrizia Trammell <patrizia.trammell@yahoo.com>
2025-04-03 19:38:55 +0200

    done one minor mistake

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,35 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers (num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num1 < num2) {
+        return num2;
+    } else {
+        return num1;
+}}
 
+//to test use console.log
+console.log(maxOfTwoNumbers (5, 10));
+console.log(maxOfTwoNumbers (22, 5));
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    let longestWord = "";
+
+    arr.forEach(function(word) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    });
+
+    return longestWord;
+}
+
+console.log(findLongestWord(words)); // should print "crocodile"
 
 
 
@@ -15,20 +37,59 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(allNumbers) {
+   let sum = 0;
+    
+    allNumbers.forEach(function (element) {
+    sum += element;     
+   });
+   return sum;
+};
+
+//console.log(sumNumbers(numbers));
 
 
-
-
+//code had many syntax mistakes had to run it to check
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+    if (arr.length === 0) 
+    return 0;
 
+    let sum = 0;
+    
+    arr.forEach(function(num) {
+        sum += num;
+    });
+
+    let average = sum / arr.length;
+    return average;
+}
+
+//console.log(averageNumbers(numbers);)
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+//Instance of i in every single loop
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+   if (arr.length === 0) {
+    return null;
+   }
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === word){ 
+        return true;
+    }
+  }
+        return false;
+}
+
+
+  
+    
+  
+
+
